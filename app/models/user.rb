0fcf74719c1b6cfa796d3828c0d5d4406042c4b3
永井達也 <Nagai-MBA@TN-MBP.local>
2017-04-16 02:06:09 +0900

    finish signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase}
 
+  #呼び出されているクラス内のメソッドをシンボルで呼び出すことができる
+  before_create  :create_remeber_token
+
   validates :name , presence: true, length: {maximum: 50}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -16,4 +19,27 @@ class User < ActiveRecord::Base
   # https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  # 新しいトークンを発行
+  # @return [string]
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # トークンを暗号化する
+  # @param [string] トークン
+  # @return [string]
+  def User.encrypt(token)
+    # tokenがnilの場合のto_sをすることで""空文字に変換することで、エラーを回避(テストのため)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  # 暗号化したトークンを作成
+  # @return [string]
+  def create_remeber_token
+    # Userクラスのremember_tokenに値を代入するためにselfを使っている
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
